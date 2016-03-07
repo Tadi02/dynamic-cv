907209dade629aa3a5c5253c70c2f7435e26a87f
Tadi <adam.tomko@gmail.com>
2016-03-07 13:48:39 +0100

    bootstrap fonts

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express();
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/fonts', express.static(__dirname + '/node_modules/bootstrap/dist/fonts'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 
 app.get('/', function (req, res) {
