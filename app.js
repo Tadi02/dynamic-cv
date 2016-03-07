a82fc3110c77e3feed3cb1d9b48cdf5548b0fe85
Tadi <adam.tomko@gmail.com>
2016-03-07 13:05:29 +0100

    use absolute path for static dirs

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
-app.use('/css', express.static('node_modules/bootstrap/dist/css'));
-app.use('/js', express.static('node_modules/bootstrap/dist/js'));
-app.use('/js', express.static('node_modules/jquery/dist'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
