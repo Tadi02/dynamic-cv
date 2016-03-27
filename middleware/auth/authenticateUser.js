1cb25c3d715d1edbfb1df0bc718661541b3af737
Tadi <adam.tomko@gmail.com>
2016-03-27 23:34:38 +0200

    comment update

--- middleware/auth/authenticateUser.js
+++ middleware/auth/authenticateUser.js
@@ -1,6 +1,6 @@
 module.exports = function () {
 
-    //Authenticate user if user data is present in request. If there is an error add error info to res. If data is valid login the user and redirect to '/'
+    //Authenticate user if user data is present in request. If there is an error add error info to res. If data is valid login the user and redirect to '/editprofile'
     return function (req, res, next) {
         console.log("Authenticate");
         return next();
