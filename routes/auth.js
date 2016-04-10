ea67886d86bbd1eee0cd38edb2254bab21f7c6b8
Tadi <adam.tomko@gmail.com>
2016-04-10 18:55:58 +0200

    auth views converted to ejs

--- routes/auth.js
+++ routes/auth.js
@@ -16,7 +16,7 @@ module.exports = function (app) {
     app.use('/register',
         preventReLogin(),
         registerUser(),
-        renderTemplate("registration")
+        renderTemplate("register")
     );
     
     
