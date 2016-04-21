7b46d9ea0a77e3bcc3b49a21e944cebd6ad9e4a3
Tadi <adam.tomko@gmail.com>
2016-04-21 10:09:14 +0200

    tech addition to activity

--- middleware/activity/addTechnologyToActivity.js
+++ middleware/activity/addTechnologyToActivity.js
@@ -1,9 +1,27 @@
+var Activity = require('../../models/activity');
+var Technology = require('../../models/technology');
+
 module.exports = function () {
 
     //Adds a new technology to an activity
     return function (req, res, next) {
-      console.log("AddTechToActivity");
-      return next();
+        if ((typeof req.params.id === 'undefined') || (typeof req.params.techId === 'undefined')) {
+            return next();
+        }
+
+        //TODO check to only add a tech once
+        Technology.findById(req.params.techId, function (err, tech) {
+            if (err) {
+                console.log("Could not add tech to activity");
+                return next();
+            }
+
+            Activity.update({_id: req.params.id}, {$push: {"technologies": tech}}, function (err) {
+                if (err) console.log("Could not add tech to activity");
+                return next();
+            });
+        });
+
     };
 
 };
