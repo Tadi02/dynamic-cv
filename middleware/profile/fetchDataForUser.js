b21090ae04429f51255cad164466f5b9a26738b2
Tadi <adam.tomko@gmail.com>
2016-04-21 22:46:45 +0200

    public link

--- middleware/profile/fetchDataForUser.js
+++ middleware/profile/fetchDataForUser.js
@@ -4,12 +4,12 @@ module.exports = function () {
 
     //Fetches user data for profile view. User id is in the request (public profile) if not looks for it in session (private page with edit links).
     //Must set flag for profile template (private or public view)
-    //TODO public view and flag
     return function (req, res, next) {
         var userId = '';
         if(typeof req.params.id === 'undefined'){
             res.tpl.public = false;
             userId = req.session.user;
+            res.tpl.publicLink = req.protocol + '://' + req.get('host') + '/profiles/' + userId;
         }else{
             res.tpl.public = true;
             userId = req.params.id;
