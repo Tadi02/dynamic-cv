c59bb778d718e8883ae508c47bca900430dbaa52
Tadi <adam.tomko@gmail.com>
2016-04-21 13:05:47 +0200

    remove tech from activity

--- middleware/activity/removeTechnologyFromActivity.js
+++ middleware/activity/removeTechnologyFromActivity.js
@@ -1,9 +1,17 @@
+var Activity = require('../../models/activity');
+
 module.exports = function () {
 
     //Removes a technology from an activity
     return function (req, res, next) {
-        console.log("RemoveTechFromActivity");
-        return next();
+        if ((typeof req.params.id === 'undefined') || (typeof req.params.techId === 'undefined')) {
+            return next();
+        }
+
+        Activity.update({_id: req.params.id}, {$pull: {"technologies": req.params.techId}}, function (err) {
+            if (err) console.log("Could not remove tech from activity");
+            return next();
+        });
     };
 
 };
